2d8a0bc1bd8f74b8692fe0afa00787eb47738746
hartley <hartley@palqee.com>
2024-04-11 21:36:06 +0100

    fix(mason-null-ls): format file

--- .config/nvim/lua/yeltrah/plugins/mason-null-ls.lua
+++ .config/nvim/lua/yeltrah/plugins/mason-null-ls.lua
@@ -1,36 +1,36 @@
 return {
-  {
-    -- Formatting and linting
-    'jay-babu/mason-null-ls.nvim',
-    event = { 'BufReadPre', 'BufNewFile' },
-    dependencies = {
-			'williamboman/mason.nvim', -- Used and configured in nvim-lspconfig.lua
+	{
+		-- Formatting and linting
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim", -- Used and configured in nvim-lspconfig.lua
 			{
-				'nvimtools/none-ls.nvim',
+				"nvimtools/none-ls.nvim",
 				dependencies = {
 					"nvimtools/none-ls-extras.nvim",
-				}
-			}
+				},
+			},
 		},
-    config = function()
-      require('mason').setup()
-      local null_ls = require('null-ls')
-      local formatting = null_ls.builtins.formatting
-      local diagnostics = null_ls.builtins.diagnostics
-			local code_actions = null_ls.builtins.code_actions
-      null_ls.setup({
-        border = require('yeltrah.settings').border,
-        sources = {
+		config = function()
+			require("mason").setup()
+			local null_ls = require("null-ls")
+			local formatting = null_ls.builtins.formatting
+			local diagnostics = null_ls.builtins.diagnostics
+			-- local code_actions = null_ls.builtins.code_actions
+			null_ls.setup({
+				border = require("yeltrah.settings").border,
+				sources = {
 					-- formatting
-          formatting.black,
-          formatting.stylua,
-          formatting.prettierd,
-          formatting.clang_format.with({
-            extra_args = { '--style', 'webkit' },
-          }),
-          formatting.shfmt.with({
-            filetypes = { 'sh', 'zsh' },
-          }),
+					formatting.black,
+					formatting.stylua,
+					formatting.prettierd,
+					formatting.clang_format.with({
+						extra_args = { "--style", "webkit" },
+					}),
+					formatting.shfmt.with({
+						filetypes = { "sh", "zsh" },
+					}),
 					formatting.terraform_fmt,
 
 					-- diagnostics
@@ -49,12 +49,12 @@ return {
 
 					-- code actions
 					require("none-ls.code_actions.eslint_d"),
-        },
-      })
-      require('mason-null-ls').setup({
-        ensure_installed = nil,
-        automatic_installation = true,
-      })
-    end,
-  },
+				},
+			})
+			require("mason-null-ls").setup({
+				ensure_installed = nil,
+				automatic_installation = true,
+			})
+		end,
+	},
 }

1887b39694f1b4000f39394829646b97611ee26a
hartley <hartley@palqee.com>
2024-04-11 21:36:21 +0100

    fix(nvim-cmp): add missing deps. remove mac file

--- .config/nvim/lua/yeltrah/plugins/nvim-cmp.lua
+++ .config/nvim/lua/yeltrah/plugins/nvim-cmp.lua
@@ -1,47 +1,59 @@
 return {
-  {
-    -- Completion
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp', -- Used and configured in nvim-lspconfig
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-cmdline',
-      'dcampos/nvim-snippy',
-      'honza/vim-snippets',
-      'dcampos/cmp-snippy',
-			'L3MON4D3/LuaSnip',
-			'onsails/lspkind.nvim'
-    },
-    config = function()
-      local cmp = require('cmp')
-      local snippy = require('snippy')
-			local luasnip = require('luasnip')
-			local lspkind = require('lspkind')
+	{
+		-- Completion
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp", -- Used and configured in nvim-lspconfig
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-calc",
+			"dcampos/nvim-snippy",
+			"honza/vim-snippets",
+			"dcampos/cmp-snippy",
+			"L3MON4D3/LuaSnip",
+			"rafamadriz/friendly-snippets",
+			"onsails/lspkind.nvim",
+		},
+		config = function()
+			local cmp = require("cmp")
+			local snippy = require("snippy")
+			local luasnip = require("luasnip")
+			local lspkind = require("lspkind")
 
-      cmp.setup({
-        enabled = function()
-          -- Disable in buffers corresponding to telescope prompt
-          return vim.api.nvim_buf_get_option(0, 'buftype') ~= 'prompt'
-        end,
-        window = {
-          documentation = cmp.config.window.bordered(),
-        },
+			local check_backspace = function()
+				local col = vim.fn.col(".") - 1
+				return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+			end
+
+			cmp.setup({
+				enabled = function()
+					-- Disable in buffers corresponding to telescope prompt
+					return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt"
+				end,
+				window = {
+					documentation = cmp.config.window.bordered(),
+				},
 				experimental = {
 					ghost_text = false,
 					native_menu = false,
 				},
 				formatting = {
 					fields = { "kind", "abbr", "menu" },
-					format = lspkind.cmp_format({ with_text = false, maxwidth = 50 })
+					format = lspkind.cmp_format({
+						with_text = false,
+						maxwidth = 50,
+						mode = "symbol_text",
+					}),
 				},
-        snippet = {
-          expand = function(args)
-            snippy.expand_snippet(args.body)
-          end,
-        },
-        preselect = cmp.PreselectMode.None,
-        mapping = cmp.mapping.preset.insert({
+				snippet = {
+					expand = function(args)
+						snippy.expand_snippet(args.body)
+					end,
+				},
+				preselect = cmp.PreselectMode.None,
+				mapping = cmp.mapping.preset.insert({
 					["<C-k>"] = cmp.mapping.select_prev_item(),
 					["<C-j>"] = cmp.mapping.select_next_item(),
 					["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
@@ -52,27 +64,27 @@ return {
 						i = cmp.mapping.abort(),
 						c = cmp.mapping.close(),
 					}),
-          ['<CR>'] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-          }),
-          ['<C-n>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif snippy.can_expand_or_advance() then
-              snippy.expand_or_advance()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<C-p>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif snippy.can_jump(-1) then
-              snippy.previous()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
+					["<CR>"] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Replace,
+					}),
+					["<C-n>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif snippy.can_expand_or_advance() then
+							snippy.expand_or_advance()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<C-p>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif snippy.can_jump(-1) then
+							snippy.previous()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 					["<Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_next_item()
@@ -101,37 +113,38 @@ return {
 						"i",
 						"s",
 					}),
-        }),
-        sources = {
-          { name = 'snippy' },
-          { name = 'nvim_lsp' },
-          { name = 'buffer' },
-          { name = 'path' },
+				}),
+				sources = {
+					{ name = "snippy" },
+					{ name = "nvim_lsp" },
+					{ name = "nvim_lua" },
+					{ name = "buffer" },
+					{ name = "path" },
 					{ name = "calc" },
 					{ name = "treesitter" },
-        },
-      })
+				},
+			})
 
-      cmp.setup.cmdline('/', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = 'buffer' },
-        },
-      })
+			cmp.setup.cmdline("/", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
 
-      cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = 'path' },
-        }, {
-          {
-            name = 'cmdline',
-            option = {
-              ignore_cmds = { 'Man', '!' },
-            },
-          },
-        }),
-      })
-    end,
-  },
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{
+						name = "cmdline",
+						option = {
+							ignore_cmds = { "Man", "!" },
+						},
+					},
+				}),
+			})
+		end,
+	},
 }

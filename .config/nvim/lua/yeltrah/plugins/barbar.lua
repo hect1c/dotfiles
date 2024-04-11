03fab53a8523c7d15934a0f30dad13852a830eed
hartley <hartley@palqee.com>
2024-04-11 23:02:56 +0100

    fix(barbar): stylua formatting

--- .config/nvim/lua/yeltrah/plugins/barbar.lua
+++ .config/nvim/lua/yeltrah/plugins/barbar.lua
@@ -1,14 +1,14 @@
 return {
-	{
-		"romgrk/barbar.nvim",
-		event = { "BufEnter" },
-		dependencies = {
-			"lewis6991/gitsigns.nvim",
-			"nvim-tree/nvim-web-devicons",
-		},
-		init = function()
-			vim.g.barbar_auto_setup = false
-		end,
-		opts = {},
-	},
+  {
+    'romgrk/barbar.nvim',
+    event = { 'BufEnter' },
+    dependencies = {
+      'lewis6991/gitsigns.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
+    opts = {},
+  },
 }

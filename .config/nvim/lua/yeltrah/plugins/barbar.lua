8e6d80fa0b454797068efe740342df166696bd8f
hartley <hartley@palqee.com>
2024-04-11 21:33:14 +0100

    fix(barbar): add gitsigns

--- .config/nvim/lua/yeltrah/plugins/barbar.lua
+++ .config/nvim/lua/yeltrah/plugins/barbar.lua
@@ -2,8 +2,12 @@ return {
 	{
 		"romgrk/barbar.nvim",
 		dependencies = {
+			"lewis6991/gitsigns.nvim",
 			"nvim-tree/nvim-web-devicons",
-			opt = true
 		},
-	}
+		init = function()
+			vim.g.barbar_auto_setup = false
+		end,
+		opts = {},
+	},
 }
